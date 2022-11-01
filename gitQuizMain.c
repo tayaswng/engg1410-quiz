ae5b238b5df8dfd85ef5253079dc9a4924075595
tayaswng <taya.wong.tor@gmail.com>
2022-11-01 13:13:08 -0400

    Modified code so that if the user does not answer 'y' to the question 'you do not know how to use Git!' is printed

--- gitQuizMain.c
+++ gitQuizMain.c
@@ -11,7 +11,7 @@ int main(void)
         printf("You know how to use Git!");
     }
     else{
-        printf("You don't know how to use Git!");
+        printf("You don't know how to use Git!"); //new line added
     }
 
     printf("\n");
